4a1d2f521adbb75321cbcbf462b10bd04f14c199
LorenBaker <LorenABaker@comcast.net>
2013-10-31 10:53:16 -0700

    Beginning of day commit: 10-31-2013

--- src/com/lbconsulting/alist_02/contentprovider/AListContentProvider.java
+++ src/com/lbconsulting/alist_02/contentprovider/AListContentProvider.java
@@ -812,4 +812,16 @@ public class AListContentProvider extends ContentProvider {
 		}
 	}
 
+	/**
+	 * A test package can call this to get a handle to the database underlying
+	 * AListContentProvider, so it can insert test data into the database. The
+	 * test case class is responsible for instantiating the provider in a test
+	 * context; {@link android.test.ProviderTestCase2} does this during the call
+	 * to setUp()
+	 * 
+	 * @return a handle to the database helper object for the provider's data.
+	 */
+	public AListDatabaseHelper getOpenHelperForTest() {
+		return database;
+	}
 }
